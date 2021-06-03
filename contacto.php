30b46adc5a538cc33f7e4f007d38693f80fca645
Francisco Cal y Mayor <67610055+descrop@users.noreply.github.com>
2021-06-02 17:23:39 -0700

    new

---
+++ contacto.php
@@ -0,0 +1,13 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+
+echo "<h2>Informacion recivida desde Educavit</h2>";
+echo "El nombre recibido es " .$nombre ."<br/>";
+echo "El nombre recibido es " .$email ."<br/>";
+echo "El nombre recibido es " .$mensaje ."<br/>";
+
+?>
