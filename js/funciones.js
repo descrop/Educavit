5616f168b853f18ee614748b1d524d29e1552631
Francisco Cal y Mayor <67610055+descrop@users.noreply.github.com>
2021-06-03 22:08:59 -0700

    cambios js

--- js/funciones.js
+++ js/funciones.js
@@ -7,7 +7,16 @@ jQuery(".hamb").click(function(e){
 e.preventDefault();
 jQuery("header .container nav").toggleClass("open");
 
-jQuery(".hamb i").toggleClass("fa-card");
+jQuery(".hamb i").toggleClass("fa-times");
 });
+
+jQuery("header .container nav a").click(function(){
+    jQuery("header .container nav").removeClass("open");
+    jQuery(".hamb i").removeClass("fa-times");
+    var dev = jQuery(this).attr("href");
+    jQuery("html,body").animate({
+        "scrollTop": jQuery(dev).offset().top
+    })
+})
 
 }
