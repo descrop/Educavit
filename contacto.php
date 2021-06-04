c6b0feb259300df1b7286893dfd12f9b61be0ed4
Francisco Cal y Mayor <67610055+descrop@users.noreply.github.com>
2021-06-04 00:06:24 -0700

    cambios

--- contacto.php
+++ contacto.php
@@ -1,27 +1,30 @@
-<?php
+<?php 
+                               if($_SERVER["REQUEST_METHOD"] == "POST"){
 
+                                $nombre = $_POST["nombre"];
+                                $email = $_POST["email"];
+                                $mensaje = $_POST["mensaje"];
 
+                                if(isset($nombre)){
+                                    if(isset($email)){
+                                        if(isset($mensaje)){
 
+                                            $para = "francisco.calymayor@avitsol.com";
+                                            $asunto = "Esto es una porueba";
+                                            $cuerpo = $nombre."\n".$email."\n".$mensaje."\n";
+                                            $adicional = "From: Noreply@francisco.com";
 
-echo "<h2>Informacion recivida desde Educavit</h2>";
-echo "El nombre recibido es " .$nombre ."<br/>";
-echo "El nombre recibido es " .$email ."<br/>";
-echo "El nombre recibido es " .$mensaje ."<br/>";
-
-if(!isset( $_POST['nombre']) || !isset($_POST['email']) || !isset($_POST['mensaje']) ) {
-	echo 'Algo salió mal. Por favor intenta de nuevo';
-    die();
-}
-	$email_from = "francisco.calymayorg@avitsol.com";
-	$email_subject = "Nuevo Lead: educavit - ".($_POST['nombre'])." - ".($_POST['nombre']);
-	$email_message ="Enviado el ".date("d/m/Y")." a las ".date("H:i")."\n\n";
-	$email_message .= "Nombre: ".stripslashes($_POST['nombre'])."\nSistema: ".stripslashes($_POST['mensaje'])."\nEmail: ".stripslashes($_POST['mensaje'])."\nWhatsapp: ";
+                                            mail($para,$asunto,$cuerpo,$adicional);
+                                            
+											echo"<div style='background-color:#38d430; font-size:1.5em; color: white; font-weight:bold; padding:5px;'>Gracias por tu tiempo. Nos pondremos en contacto contigo a la brevedad.</div><p>&nbsp;</p>";
+                                         ?>
 	
+                                        <?php
+                                      
+                                        }
+                                    }
+                                }
 
-	$headers = 'From: '.$email_from."\r\n" .
-   'Reply-To: '.$email_from."\r\n" ;
-
-	mail('francisco.calymayorg@avitsol.com', $email_subject, $email_message, $headers);
-	echo "<div style='background-color:#38d430; font-size:1.5em; color: white; font-weight:bold; padding:5px;'>Gracias por tu tiempo. Nos pondremos en contacto contigo a la brevedad.</div><p>&nbsp;</p>";
-	die();
-?>
+                               }
+                              
+                               ?>
--- index.php
+++ index.php
@@ -297,23 +297,22 @@
 
                                             $para = "francisco.calymayor@avitsol.com";
                                             $asunto = "Esto es una porueba";
-                                            $cuerpo = $nombre."\n".$email."\n";
+                                            $cuerpo = $nombre."\n".$email."\n".$mensaje."\n";
                                             $adicional = "From: Noreply@francisco.com";
 
                                             mail($para,$asunto,$cuerpo,$adicional);
+                                            
+											echo"<div style='background-color:#38d430; font-size:1.5em; color: white; font-weight:bold; padding:5px;'>Gracias por tu tiempo. Nos pondremos en contacto contigo a la brevedad.</div><p>&nbsp;</p>";
                                          ?>
-                                         echo'<script type="text/javascript">
-                                     alert("Mensaje enviado");
-                            window.location.href="index.php";
-                                            </script>';
-                                         
+	
                                         <?php
-                                        
+                                      
                                         }
                                     }
                                 }
 
                                }
+                              
                                ?>
                             </div>
                             </div>
